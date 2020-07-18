fdc74a9a3d549d0524d69d3c59ebb0d908960a02
Emmanuel Girardin <emmanuel.girardin@outlook.com>
2020-07-18 09:25:54 +0200

    MOD(main_gui): add glow_effect

--- main_gui.py
+++ main_gui.py
@@ -23,6 +23,7 @@ class FractalExplorer:
         self.pos_mandel_xy = constant_xy
 
         self.pos_mouse_mandel_xy = 0, 0
+        self.light_effect = -1
 
     def update_julia_hits(self):
         self.julia_hits = juliaset.juliaset(self.dim_xy, self.pos_julia_xy, self.zoom, self.r_mat, self.pos_mandel_xy)
@@ -30,23 +31,42 @@ class FractalExplorer:
     def update_mandel_hits(self):
         self.mandel_hits = juliaset.mandelbrotset(self.dim_xy, self.pos_mandel_xy, self.zoom, self.r_mat)
 
+    def glow_effect(self, img):
+        img2 = cv.GaussianBlur(img, ksize=(21,21), sigmaX=5, sigmaY=5)
+        out = img2
+        out[img>img2] = img[img>img2]
+        return out
+
     def color_map(self, hits):
-        hits = (np.log(hits+1)*20).astype(np.uint16)
-        maxhit = np.max(hits)
+        dB = lambda x: (20*np.log(x+1)).astype(np.uint16)
+        hits = dB(hits)
+        N = np.max(hits)+1
         # BRG colors
-        color_map = np.array([
-            [  0,  0,  0],
-            [  0,255,255],
-            [255,  0,  0],
-            [255,255,255],
+        color_map_0 = np.array([
+            [0, 0, 0],
+            [255, 255, 0],
+            [0, 255, 255],
+            [63, 195, 0],
+            [255, 255, 255],
         ], dtype=np.uint8)
-        color_map = cv.resize(color_map, dsize=(3, maxhit+1), interpolation=cv.INTER_LINEAR)
-        color_map.shape = (color_map.shape[0], 1, 3)
+        color_map_0 = cv.resize(color_map_0, dsize=(3, N), interpolation=cv.INTER_LINEAR)
+        color_map_1 = np.array([
+            [0, 0, 255],
+            [255, 255, 255],
+        ], dtype=np.uint8)
+        color_map_1 = cv.resize(color_map_1, dsize=(3, N), interpolation=cv.INTER_LINEAR)
+        color_switch = np.zeros(shape=(N, 1))
+        color_switch[int(self.light_effect*N)] = 1
+        color_map = color_map_0*(1-color_switch) + color_map_1*color_switch
+        # if self.light_effect >= 0:
+        #     color_map[int(maxhit*self.light_effect), :] = [255, 255, 255]
+        color_map.shape = (N, 1, 3)
         return juliaset.apply_color_map(hits, color_map)
 
     def update_julia_display(self):
         tic = time.time()
         self.julia_display = self.color_map(self.julia_hits)
+        self.julia_display = self.glow_effect(self.julia_display)
         print(time.time()-tic)
 
     def update_mandel_display(self):
@@ -86,13 +106,14 @@ class FractalExplorer:
         W, H = self.dim_xy
         if x < W:
             # mouse on juliaset
-            pass
+            self.light_effect = x/W
+            self.update_julia_display()
         else:
             # mouse on mandelbrot
             pos_screen_xy = (self.mousePosition[0] - W, self.mousePosition[1])
             self.pos_mouse_mandel_xy = juliaset.screen_space_to_cartesian(
                 self.dim_xy, self.pos_mandel_xy, self.zoom, self.r_mat, pos_screen_xy)
-            self.display()
+        self.display()
 
 
 
